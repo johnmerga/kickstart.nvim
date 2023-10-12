3e82144d85b3b012c7e09023c357ae41578b44bd
johnmerga <johnmerga@yahoo.com>
2023-10-12 15:12:31 +0300

    update nvim

---
+++ lua/john/plugins/no-configs.lua
@@ -0,0 +1,14 @@
+return {
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+}
